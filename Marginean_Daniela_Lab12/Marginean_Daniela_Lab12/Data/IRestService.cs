f366665a0237a1f946853a77374b1371b4a7fdfb
Daniela Marginean <dana.marginean@gmail.com>
2022-01-17 00:02:37 +0200

    Laborator 12

---
+++ Marginean_Daniela_Lab12/Marginean_Daniela_Lab12/Data/IRestService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Retegan_Andra_Lab12.Models;
+
+namespace Retegan_Andra_Lab12.Data
+{
+    public interface IRestService
+    {
+        Task<List<ShopList>> RefreshDataAsync();
+        Task SaveShopListAsync(ShopList item, bool isNewItem);
+        Task DeleteShopListAsync(int id);
+    }
+}
